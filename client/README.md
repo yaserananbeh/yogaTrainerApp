7673c77cc73d38b8a969914932ff75fc541ca28e
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-02-06 02:39:48 +0200

    setup the client project structure

---
+++ client/README.md
@@ -0,0 +1,11 @@
+# Yoga Trainers App Client Side
+
+- The user can view list of yoga trainers 
+- The user can login\register to the website 
+- The user can search from a list of trainers
+- user can request a reservation from any trainer
+###  ---------------------
+- trainer can login/register 
+- trainer can receive a appointments 
+- trainer can accept appointments 
+- trainer can reject appointments
---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/App.scss";
+
+function App() {
+  return (
+    <div className="AppMainContainer">
+      <i className="fab fa-500px"></i>
+      <h5 className="testHeading">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores
+        odio nesciunt eaque, sed a eius nostrum! Voluptates optio sit quidem at.
+        Animi repellendus voluptas corporis tenetur a aut possimus?
+      </h5>
+    </div>
+  );
+}
+
+export default App;
