cef31e32b11fe5a15818c79f47010187fc37ed0a
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-02-09 16:39:07 +0200

    user profile page future work

---
+++ client/src/pages/UserProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UserReservations from "../components/UserReservations";
+function userProfile() {
+  return (
+    <div>
+      <UserReservations />
+    </div>
+  );
+}
+
+export default userProfile;
