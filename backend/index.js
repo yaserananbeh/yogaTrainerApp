43a672aad86561a98ca0ef9fc9adcd6d05d79ec1
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-02-12 02:16:05 +0200

    change the db connection to atlas

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ let mongoose = require("mongoose");
 let apiRoutes = require("./api-routes.js");
 
 // connect to mongoose
-mongoose.connect("mongodb://localhost/bookingApp", { useNewUrlParser: true });
+mongoose.connect("mongodb://yogaadmin:yogaadmin@cluster0-shard-00-00.obsku.mongodb.net:27017,cluster0-shard-00-01.obsku.mongodb.net:27017,cluster0-shard-00-02.obsku.mongodb.net:27017/test?ssl=true&replicaSet=atlas-4hu5m5-shard-0&authSource=admin&retryWrites=true&w=majority");
 
 // if there's a env file it will get the port number from it, other wise will start the server at 4000 port
 const PORT = process.env.PORT || 4000;
